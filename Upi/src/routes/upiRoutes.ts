095a4130116ac83364b013c128447ebf7841403a
dijul2346 <146112003+dijul2346@users.noreply.github.com>
2025-11-21 16:36:28 +0530

    change in routes

--- Upi/src/routes/upiRoutes.ts
+++ Upi/src/routes/upiRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { PaymentController } from "../controllers/payment.controller";
 const router = Router();
-router.post('/pay', PaymentController.createTransaction);
+router.post('/pay/create', PaymentController.createTransaction);
 router.get('/pay/verify/:linkId', PaymentController.verifyPayment);
 export default router;
